b808c9aa38728a6d6f95bf6a7f50ed5c16f2088b
code-lova <ebinuga@hotmail.com>
2023-09-13 19:07:36 +0100

    wk01-task

---
+++ main.js
@@ -0,0 +1,38 @@
+var adult = true;
+
+if (adult){
+    var myName = "bob"
+    let age = 30
+}
+
+let names = ["Bob", "Sue", "Jorge", "Svetlana"]
+names[2] = "George"
+names.push("Grace")
+names.pop()
+
+const fruits =[]
+fruits.push("Orange", "mango", "Grape", "Guava")
+
+const hello = "Hello..!!"
+const world = "World"
+const complexString = `${hello} ${world}` 
+
+let num1 = 100;
+let num2 = '10';
+let sum = num1 + parseInt(num2);
+console.log(sum)
+
+
+console.log(complexString)
+
+console.log(fruits.length)
+console.log(fruits)
+console.log(fruits[3])
+console.log(Object.keys(fruits))
+
+
+
+
+console.log(names)
+
+console.log(myName)
